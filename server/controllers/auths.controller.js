d0eba70df6df86086d75ef28f4fb16e6abc58c6d
Long <longjax1401@gmail.com>
2021-04-03 00:10:52 +0700

    add register api

--- server/controllers/auths.controller.js
+++ server/controllers/auths.controller.js
@@ -1,5 +1,11 @@
+const bcrypt = require('bcrypt');
+
+const { User } = require('../models')
+
+let users = []
+
 module.exports.index = (req, res) => {
-    res.send('LoginFrom')
+    res.redirect('/auths/login')
 }
 
 module.exports.loginForm = (req, res) => {
@@ -11,13 +17,60 @@ module.exports.login = (req, res) => {
 }
 
 module.exports.registerForm = (req, res) => {
-    res.send('/RegisterForm')
+    res.send('RegisterForm')
 }
 
-module.exports.register = (req, res) => {
-    res.redirect('/register')
+module.exports.register = async (req, res) => {
+    let { username, password, email, name, phone } = req.body
+    try {
+        if (![ username, password, email, name, phone ].every(Boolean)) {
+            throw new Error('Invalid arguments required');
+        }
+        else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            User.findOne({username})
+                .then(user => {
+                    if (user) {
+                        throw new Error({
+                            code: 303,
+                            message: "There is an existing user",
+                        })
+                    }
+                    else {
+                        let user = {
+                            username,
+                            password: hashedPassword,
+                            name,
+                            email,
+                            phone
+                        }
+                        users.push(user)
+                        return res.json({ 
+                            code : 200, 
+                            message : "thêm người dùng thành công", 
+                            data : user
+                        })
+                    }
+                })
+                .catch(err => res.json({ 
+                    code: err.code || 404, 
+                    message: err.message,
+                }))
+        }
+    }
+    catch (err) {
+        res.json({ 
+            code: err.code || 400, 
+            message: err.message,
+        })
+    }
 }
 
 module.exports.logout = (req, res) => {
-    res.redirect('/')
+    let username = req.session.username;
+    users.map(user => user.username !== username)
+    res.json({ 
+        code: 0,
+        message: `${username} has been logged out`,
+    })
 }
