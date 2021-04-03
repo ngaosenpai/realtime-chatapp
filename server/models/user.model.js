72ca122b6a35dfa502160e162a70fb8cc9c5844e
Long <longjax1401@gmail.com>
2021-04-04 00:45:23 +0700

    add hashing password in model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,27 +1,49 @@
-let mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-let userSchema = new mongoose.Schema({
-    usernames: String,
-	password: {
-		type: String,
-		require: true,
+const UserSchema = new mongoose.Schema({
+	locals: {
+		username: {
+			type: String,
+			require: true,
+			index: true,
+			unique: true,
+			sparse: true
+		},
+		password: {
+			type: String,
+			require: true,
+		},
+		email: {
+			type: String,
+			require: true,
+			index: true,
+			unique: true,
+			sparse: true
+		},
+		name: String,
+		image: String,
+		phone: String,
 	},
-    name: String,
-	image: String,
-    email: {
-		type: String,
-		require: true,
-		unique: true,
-	},
-    phone: String,
     updated: { 
 		type: Date, 
 		default: Date.now 
 	},
-    createdAt: Date
+    createdAt: { 
+		type: Date, 
+		default: Date.now 
+	}
 
 });
 
-let User = mongoose.model("User", userSchema, "users")
+UserSchema.pre('save', async function(next) {
+	let user = this;
+	const salt = await bcrypt.genSalt(10)
+	user.locals.password = await bcrypt.hash(user.locals.password, salt)
+	next();
+})
+
+
+let User = mongoose.model("User", UserSchema, "users")
 //first para is name of model, second is schema, final is name of collection in db
 module.exports = User;
