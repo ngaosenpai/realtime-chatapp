2636e8af69eeec7a2d30c622ee7abab137ec7981
Long <longjax1401@gmail.com>
2021-04-11 22:48:59 +0700

    add register redux saga and things

--- client/src/saga/rootSaga.js
+++ client/src/saga/rootSaga.js
@@ -4,12 +4,14 @@ import { watchJwt } from './authSaga'
 import { watchFetchSession } from './sessionSaga'
 import { watchKeepSession } from "./authSessionSaga"
 import { watchLogin } from "./loginSaga"
+import { watchRegister } from "./registerSaga"
 
 export default function* rootSaga(){
     yield all([
         watchJwt(),
         watchFetchSession(),
         watchKeepSession(),
-        watchLogin()
+        watchLogin(),
+        watchRegister()
     ])
 }
