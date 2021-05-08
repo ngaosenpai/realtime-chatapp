b03034c985f41bf8d020de336a42480c1f92d489
Long <longjax1401@gmail.com>
2021-05-08 12:20:07 +0700

    change bunch of stuffs

--- server/server.js
+++ server/server.js
@@ -80,9 +80,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.SERVER_POST || 5000;
+const PORT = process.env.SERVER_POST || 4000;
 
-server.listen( PORT, () => {
+server.listen( PORT, "0.0.0.0", () => {
     console.log(`server listening on port ${PORT}: ${process.env.REACT_APP_SERVER_URL}/`);
 })
 
